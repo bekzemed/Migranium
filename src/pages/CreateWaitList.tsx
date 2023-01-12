4925a04d4a19da73d6c1de46b407e09b427cd319
bereket zemed <bereket@seekmake.tech>
2023-01-12 22:37:42 +0300

    update text size

--- src/pages/CreateWaitList.tsx
+++ src/pages/CreateWaitList.tsx
@@ -1,6 +1,6 @@
 const CreateWaitList = () => {
   return (
-    <div className="py-14 px-4 text-center lg:px-20 2xl:px-80 md:w-[800px] m-auto">
+    <div className="py-14 px-4 text-center lg:px-20 2xl:px-36 md:w-[800px] m-auto">
       <span className="block text-3xl pb-8">
         Sign up now and create your waitlist
       </span>
