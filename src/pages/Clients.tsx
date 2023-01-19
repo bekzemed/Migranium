fb0f06b86e47370d37803ac79cec507abe72620b
bereket zemed <bereket@seekmake.tech>
2023-01-19 21:17:05 +0300

    apply dots to scroll

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -10,7 +10,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const settings = {
-  dots: false,
+  dots: true,
   infinite: true,
   speed: 500,
   slidesToShow: 4,
