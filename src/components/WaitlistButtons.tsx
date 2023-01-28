edb3a10959064bf3f335c12f99df5c2dbbcaff49
bereket zemed <bereket@seekmake.tech>
2023-01-28 11:16:38 +0300

    fix station background color

--- src/components/WaitlistButtons.tsx
+++ src/components/WaitlistButtons.tsx
@@ -5,19 +5,19 @@ const WaitlistButtons = () => {
     <div className="flex justify-between lg:justify-start text-xs mb-6">
       <button
         type="button"
-        className="text-white bg-secondary mr-2 hover:bg-bright focus-visible:outline-none focus:outline-none font-medium rounded-full w-full lg:w-[150px] px-5 py-2.5 text-center mb-2"
+        className="text-white bg-secondary mr-2 hover:bg-secondary focus-visible:outline-none focus:outline-none font-medium rounded-full w-full lg:w-[150px] px-5 py-2.5 text-center mb-2"
       >
         All stations
       </button>
       <button
         type="button"
-        className="text-black bg-white mr-2 border-bright hover:bg-bright hover:text-white focus-visible:outline-none focus:outline-none font-medium rounded-full w-full lg:w-[150px] px-5 py-2.5 text-center mb-2"
+        className="text-black bg-white mr-2 border-secondary hover:bg-secondary hover:text-white focus-visible:outline-none focus:outline-none font-medium rounded-full w-full lg:w-[150px] px-5 py-2.5 text-center mb-2"
       >
         Station 1
       </button>
       <button
         type="button"
-        className="text-black bg-white border-bright hover:bg-bright hover:text-white focus-visible:outline-none focus:outline-none font-medium rounded-full w-full lg:w-[150px] px-5 py-2.5 text-center mb-2"
+        className="text-black bg-white border-secondary hover:bg-secondary hover:text-white focus-visible:outline-none focus:outline-none font-medium rounded-full w-full lg:w-[150px] px-5 py-2.5 text-center mb-2"
       >
         Station 2
       </button>
