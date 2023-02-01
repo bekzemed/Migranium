57b7ae22d7fb2f6a83b0dd5918ee7fd61abcdb48
bereket zemed <bereket@seekmake.tech>
2023-02-01 10:34:18 +0300

    update user page

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,51 +1,43 @@
-import { useState } from "react";
-import NavBar from "./NavBar";
-import signup from "../assets/signup.svg";
-import google from "../assets/google.svg";
+import { useNavigate } from "react-router-dom";
 import welcome from "../assets/welcome.svg";
-import { Link, useNavigate } from "react-router-dom";
+import logo from "../assets/logo.svg";
 
 export const User = () => {
-  const [show, setShow] = useState(false);
   const navigate = useNavigate();
 
   return (
-    <div>
-      <NavBar show={show} onDone={() => setShow(!show)} hide={true} />
-      <div className="bg-primary px-4 pt-[62px] lg:pt-[70px] text-center h-screen lg:flex lg:justify-center dark:text-black">
-        <div className="flex flex-col h-full justify-center lg:mr-10 2xl:mr-16">
-          <span className="font-black text-2xl pb-5 block lg:pb-3 whitespace-nowrap">
-            Welcome to Migranium
+    <div className="bg-primary px-4 lg:px-0 text-center h-screen lg:flex lg:justify-center dark:text-black">
+      <div className="flex flex-col h-full justify-center lg:mr-10 2xl:mr-16">
+        <span className="font-black text-2xl pb-5 block lg:pb-3 whitespace-nowrap">
+          Welcome to Cashexs
+        </span>
+        <span className="block pb-5 text-xs whitespace-nowrap">
+          <span className="block mb-3">
+            <span className="text-primary text-sm">5 people waiting</span>
           </span>
-          <span className="block pb-5 text-xs whitespace-nowrap">
-            <span className="block mb-3">
-              to queue in{" "}
-              <span className="text-primary text-sm">Cashex - Station 1</span>{" "}
-              click to continue
-            </span>
-            <span>
-              {" "}
-              <span className="text-primary text-sm">40 - 60 min</span>{" "}
-              estimated wait{" "}
-              <span className="text-primary text-sm"> 1 - 2</span> people
-              waiting
-            </span>
+          <span className="text-sm">
+            Approximately <span className="text-primary">40 - 50 mins</span>{" "}
+            wait time
           </span>
+        </span>
 
-          <button
-            type="button"
-            className="text-white bg-secondary hover:bg-bright focus-visible:outline-none focus:outline-none font-medium rounded-full text-sm w-full px-5 py-2.5 text-center"
-            onClick={() => navigate("home-page")}
-          >
-            Join waitlist
-          </button>
+        <button
+          type="button"
+          className="text-white bg-secondary hover:bg-bright focus-visible:outline-none focus:outline-none font-medium rounded-full text-sm w-full px-5 py-2.5 text-center"
+          onClick={() => navigate("home-page")}
+        >
+          Join waitlist
+        </button>
+        <div className="text-center flex items-center text-sm justify-center mt-6">
+          <span className="mr-2">Powered by</span>
+          <span className="text-primary migranium-font flex items-center">
+            <img src={logo} alt="Logo" className="w-[20px]" />
+            migranium
+          </span>
         </div>
-        <img
-          className="hidden lg:block w-[400px]"
-          src={welcome}
-          alt="Welcome"
-        />
       </div>
+
+      <img className="hidden lg:block w-[400px]" src={welcome} alt="Welcome" />
     </div>
   );
 };
