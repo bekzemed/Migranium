71c091df7bb92b0174ac95c5b09981a432142875
bereket zemed <bereket@seekmake.tech>
2023-02-08 13:20:43 +0300

    match upgrade profile theme

--- src/components/MigraniumPlans.tsx
+++ src/components/MigraniumPlans.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useAppSelector } from "../redux/hooks";
 
 const MigraniumPlans = ({ migraniumPlans, setShowPlan, setPlanType }: any) => {
+  const theme = useAppSelector((state) => state.theme.backgroundTheme);
+  const selected = useAppSelector((state) => state.theme.selected);
+  const textColor = useAppSelector((state) => state.theme.textColor);
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
       {migraniumPlans.map((plans: any, index: number) => (
@@ -23,16 +28,32 @@ const MigraniumPlans = ({ migraniumPlans, setShowPlan, setPlanType }: any) => {
             <span className="block text-xs mb-3">
               {migraniumPlans.length !== index + 1 && "Starts at"}
             </span>
-            <span className="block text-3xl text-primary whitespace-nowrap">
+            <span
+              style={selected === 10 ? { color: textColor } : {}}
+              className={`block ${
+                textColor === "text-theme0" || textColor === "text-theme1"
+                  ? "text-black"
+                  : textColor
+              } text-3xl whitespace-nowrap`}
+            >
               {plans.payment}
             </span>
           </div>
           <button
             type="button"
+            style={
+              selected === 10 && !plans.currentPlan
+                ? { backgroundColor: theme }
+                : {}
+            }
             className={`${
               plans.currentPlan
                 ? "bg-gray-200 text-black "
-                : "bg-secondary text-white"
+                : `${
+                    theme === "bg-theme0" || theme === "bg-theme1"
+                      ? "bg-black"
+                      : theme
+                  } text-white`
             }  rounded-full focus-visible:outline-none focus:outline-none text-sm mb-4`}
             disabled={plans.currentPlan}
             onClick={() => {
@@ -45,7 +66,15 @@ const MigraniumPlans = ({ migraniumPlans, setShowPlan, setPlanType }: any) => {
           <div className="flex flex-col text-xs">
             <span className="mb-3">Plan highlights:</span>
             {plans.planHighlight.map((highlight: String, index: number) => (
-              <div className="text-primary flex flex-col" key={index}>
+              <div
+                style={selected === 10 ? { color: textColor } : {}}
+                className={`${
+                  textColor === "text-theme0" || textColor === "text-theme1"
+                    ? "text-black"
+                    : textColor
+                } flex flex-col`}
+                key={index}
+              >
                 <span className="mb-2">{highlight}</span>
               </div>
             ))}
