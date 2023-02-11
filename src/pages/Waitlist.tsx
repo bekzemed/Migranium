0f0f8d5f2e28bc613bee346caf9e2293c626bfdd
bereket zemed <bereket@seekmake.tech>
2023-02-11 22:45:21 +0300

    update padding

--- src/pages/Waitlist.tsx
+++ src/pages/Waitlist.tsx
@@ -159,8 +159,8 @@ const WaitList = () => {
 
               <WaitlistButtons />
 
-              <div className="bg-white rounded-lg px-2 py-4 mb-4">
-                <table className="w-full mb-4">
+              <div className="bg-white rounded-lg px-2 py-4">
+                <table className="w-full">
                   <thead>
                     <tr className="opacity-40 dark:text-black text-xs border-b border-gray-300">
                       <th></th>
